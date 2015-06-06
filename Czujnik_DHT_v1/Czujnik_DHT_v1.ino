2e5de0dc258fd713180ec51c916321c205b4cfd5
Voocodo <skamajster@gmail.com>
2015-06-06 22:22:36 +0200

    Update Czujnik_DHT_v1.ino

--- Czujnik_DHT_v1/Czujnik_DHT_v1.ino
+++ Czujnik_DHT_v1/Czujnik_DHT_v1.ino
@@ -2,17 +2,13 @@
 // 6.06.2015 Bezprzewodowy czujnik 433 MhZ
 
 
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
 
 #include "DHT.h"
 
 #define DHTPIN 2     // what pin we're connected to
 
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11 
+
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 // Connect pin 1 (on the left) of the sensor to +5V
 // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
@@ -20,15 +16,9 @@
 // Connect pin 2 of the sensor to whatever your DHTPIN is
 // Connect pin 4 (on the right) of the sensor to GROUND
 // Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-
 // Initialize DHT sensor for normal 16mhz Arduino
 DHT dht(DHTPIN, DHTTYPE);
-// NOTE: For working with a faster chip, like an Arduino Due or Teensy, you
-// might need to increase the threshold for cycle counts considered a 1 or 0.
-// You can do this by passing a 3rd parameter for this threshold.  It's a bit
-// of fiddling to find the right value, but in general the faster the CPU the
-// higher the value.  The default for a 16mhz AVR is a value of 6.  For an
-// Arduino Due that runs at 84mhz a value of 30 works.
+
 // Example to initialize DHT sensor for Arduino Due:
 //DHT dht(DHTPIN, DHTTYPE, 30);
 
@@ -38,14 +28,17 @@ DHT dht(DHTPIN, DHTTYPE);
 void setup() {
   Serial.begin(9600); 
   Serial.println("DHTxx test!");
- 
+  // Dht:
   dht.begin();
+  
   //Rfid:
    pinMode(rfTransmitPin, OUTPUT);     
    pinMode(ledPin, OUTPUT);    
 }
 
 void loop() {
+  
+  //Rfid:
   
   for(int i=4000; i>5; i=i-(i/3)){
      digitalWrite(rfTransmitPin, HIGH);     //Transmit a HIGH signal
@@ -57,7 +50,7 @@ void loop() {
      delay(i);                            //Variable delay
    }
  
- 
+ //DHT:
   // Wait a few seconds between measurements.
   delay(2000);
 
