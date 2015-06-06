85061705c8e42bf7f25fea9266f6ff7e204d7a89
Voocodo <skamajster@gmail.com>
2015-06-06 22:53:19 +0200

    Wirtualwire

--- Mega_Odbiornik_v1/Mega_Odbiornik_v1.ino
+++ Mega_Odbiornik_v1/Mega_Odbiornik_v1.ino
@@ -7,31 +7,41 @@
      Description: A simple sketch used to test RF transmission/receiver.          
  ------------------------------------------------------------- */
 
- #define rfReceivePin A0  //RF Receiver pin = Analog pin 0
- #define ledPin 13        //Onboard LED = digital pin 13
+
 
  unsigned int data = 0;   // variable used to store received data
  const unsigned int upperThreshold = 70;  //upper threshold value
  const unsigned int lowerThreshold = 50;  //lower threshold value
+ 
+#include <VirtualWire.h>
+
 
  void setup(){
-   pinMode(ledPin, OUTPUT);
-   Serial.begin(9600);
+    vw_set_ptt_inverted(true); // Required for DR3100
+    vw_set_rx_pin(12);
+    vw_setup(4000);  // Bits per sec
+    pinMode(13, OUTPUT);
+
+    vw_rx_start();       // Start the receiver PLL running
  }
 
  void loop(){
-   delay(100);
-   data=analogRead(rfReceivePin);    //listen for data on Analog pin 0
-   
-    if(data>upperThreshold){
-     digitalWrite(ledPin, LOW);   //If a LOW signal is received, turn LED OFF
-     Serial.println(data);
-   }
-   
-   if(data<lowerThreshold){
-     digitalWrite(ledPin, HIGH);   //If a HIGH signal is received, turn LED ON
-     Serial.println(data);
+ uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buf, &buflen)) // Non-blocking
+    {
+      if(buf[0]=='1'){
+
+  
+   digitalWrite(13,1);
+      }  
+   if(buf[0]=='0'){
+  digitalWrite(13,0);
+    }
+
+}
      
      
-   }
+   
  }
