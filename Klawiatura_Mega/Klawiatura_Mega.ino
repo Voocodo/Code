403232fbe2e9ba2140f364ca11d304141cb3dd0d
Voocodo <skamajster@gmail.com>
2015-09-11 17:38:23 +0200

    Kod pin dziala

--- Klawiatura_Mega/Klawiatura_Mega.ino
+++ Klawiatura_Mega/Klawiatura_Mega.ino
@@ -1,36 +1,35 @@
 /*
-
 1.09.2015
+10.09.2015 leniwy start
+11.09.2015 kod pin dziala
 */
 
+//------------------Libraries------------------//
 #include <Keypad.h>
 #include <LiquidCrystal.h>
+//---------------------------------------------//
 
 
-
-
-////Led:
+//-------------------Led Pins-------------------//
 int greenLed = 2;
 int redLed = 3;
 int buzzer = 4;
-  //LCD:
-  LiquidCrystal lcd(30, 31, 32, 33, 34, 35);
+//---------------------------------------------//
 
+
+//---------------------LCD----------------------//
+LiquidCrystal lcd(30, 31, 32, 33, 34, 35); 
+boolean lcdFade=1;
+unsigned long lcdFadeTime =0;
+  //LCD pin name 	RS 	EN 	DB4 	DB5 	DB6 	DB7
+  //Arduino pin  	30 	31 	32 	33 	34 	35
+//---------------------------------------------//
 
 /////////Keypad:
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
 boolean customCheck =0;
-char kodPin[4] = {'1','4','7','8'};
-int pozycja =0;
-int proba=0;
-
 char klawisz =0;
-
-//Kod pin
-byte enterPinModeActive=0;
-
-
 
 //define the cymbols on the buttons of the keypads
 char hexaKeys[ROWS][COLS] = {
@@ -39,72 +38,77 @@ char hexaKeys[ROWS][COLS] = {
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
-
 byte rowPins[ROWS] = {22, 23, 24, 25}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {26, 27, 28, 29}; //connect to the column pinouts of the keypad
-
 //initialize an instance of class NewKeypad
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
-//Buzzer
 
+//Kod pin:
+byte enterPinModeActive=0;
+char kodPin[4] = {'1','4','7','8'};
+int pozycja =0;
+int proba=0;
+byte statusUzbrojenia=1; // 1 - uzbrojony, 0-rozbrojony
+
+//Buzzer
 unsigned long buzzer_time = 0;
 unsigned long aktualnyCzas =0;
 /////////////////////////////////////////////
 
 
-void enterPinMode()
+void enterPinMode() //Sprawdza poprawność kodu PIN
 {
-    digitalWrite(greenLed,LOW);
-    digitalWrite(redLed,HIGH);
-    proba=proba+1;
-    lcd.print("*");
+    proba=proba+1; //Zlicza ilość prób wpisania kodu
+    lcd.print("*"); //Wyświetla gwiazdkę zamiast znaku
     
-    
-    Serial.println("Enter Pin Mode");
-    if (klawisz == kodPin[pozycja])
+    if (klawisz == kodPin[pozycja]) //Zmienna pozycja wskazuje poprawność wpisywanego kodu
     {
-      Serial.print("Klawisz:");
-      Serial.println(klawisz);
-      Serial.print("Kod Pin:");
-      Serial.println(kodPin[pozycja]);
-      Serial.print("Pozycja");
-      Serial.println(pozycja);
-      
-      pozycja = pozycja +1;
-      
+      pozycja = pozycja +1;   
     }
-    
     else pozycja =0;
     
-    if (pozycja == 3 && proba == 4)
-    {
-      Serial.println("Otwieram");
-     lcd.clear();
-    lcd.write("Otwarte!"); 
-    }
-   
-   if (proba==4 && proba<4)
+    
+       if (proba==4 && pozycja<4) //Jeśli kod PIN jest zły
    {
+     lcd.clear();
+     lcd.write ("Zly kod PIN");
      enterPinModeActive=0;
    }
     
+    
+    if (pozycja == 4 && proba == 4) //Pin OK
+    {
+      enterPinModeActive=0;
+      statusUzbrojenia=!statusUzbrojenia;
+    
+           if (statusUzbrojenia==0)
+        {
+         lcd.clear();
+         lcd.write("Alarm nieaktywny!");
+         digitalWrite(buzzer,HIGH);
+         buzzer_time=millis()+1000; // Włacza buzzer na sekundę oraz zostawia ekran na 3 sekundy
+         lcdFadeTime=millis()+3000;
+         lcdFade=1;
+       }
+       
+         if (statusUzbrojenia==1)
+       {
+         lcd.clear();
+         lcd.write("Alarm aktywny!"); 
+         digitalWrite(buzzer,HIGH);
+         buzzer_time=millis()+1000; // Włacza buzzer na sekundę oraz zostawia ekran na 3 sekundy
+         lcdFadeTime=millis()+3000;
+         lcdFade=1;
+       }
+  }
 }
 
-
-
-
-
-
-
-
-
 ////////////////////////////////////////////
+
 void setup(){
   Serial.begin(9600);
-  //LCD:
-    lcd.begin(20, 4);
-  //
+  lcd.begin(20, 4);
   
   pinMode(redLed, OUTPUT);
   pinMode(greenLed, OUTPUT);
@@ -113,66 +117,80 @@ void setup(){
   digitalWrite(redLed,HIGH);
   digitalWrite(greenLed,HIGH);
   digitalWrite(buzzer,LOW);
-  lcd.setCursor(0,0);
-  lcd.print("Wpisz kod PIN:");
-  lcd.setCursor(14,0);
-  
-  //LCD pin name 	RS 	EN 	DB4 	DB5 	DB6 	DB7
-  //Arduino pin  	30 	31 	32 	33 	34 	35
-  ///
 }
   
 void loop(){
-
   
-  char customKey = customKeypad.getKey();
+// Sprawdza status alarmu i sygnalizuje go diodami:
+  if (statusUzbrojenia==0)
+  {
+   digitalWrite(redLed,HIGH); 
+   digitalWrite(greenLed,LOW); 
+  }
   
-  if (customKey){
-    digitalWrite(buzzer,HIGH);
-    buzzer_time=millis()+200;
-  //Serial.println(customKey);
-  klawisz=customKey;
-  Serial.println(klawisz);
-  Serial.println(customCheck);
-  lcd.write(klawisz);
-  
-customCheck = customKeypad.keyStateChanged();
+  if (statusUzbrojenia==1)
+  {
+   digitalWrite(redLed,LOW); 
+   digitalWrite(greenLed,HIGH); 
   }
   
   
- aktualnyCzas = millis();
+ //Sprawdź czy nacisnieto nowy klawisz:  
+char customKey = customKeypad.getKey();
+   
+//Jesli tak, zapikaj i wypisz go na LCD:   
+  if (customKey)
+  {
+    Serial.println("FunkcjaCustomKey");
+    digitalWrite(buzzer,HIGH);
+    buzzer_time=millis()+200; // Czas do wylaczenia
+
+  klawisz=customKey;
+  Serial.println(klawisz);
+  //customCheck = customKeypad.keyStateChanged(); // Czy status klawisza sie zmienil?
   
-  if (aktualnyCzas>=buzzer_time)
+    if (enterPinModeActive==1 && klawisz!='#') //Jeśli wpisywany jest kod pin, następuje przekierowanie
+    {
+    //Serial.println("Funckja enterpin");
+    enterPinMode();  
+    }
+  
+  //Tu zapada decyzja co zrobić dalej:
+  switch (klawisz) {
+   case '*': 
+   enterPinModeActive=1;
+   proba=0;
+   lcd.clear();
+   lcd.write("Wpisz kod:");
+   break;
+   
+   case '#':
+   lcd.clear();
+  lcd.print("Ekran startowy.");
+  lcd.setCursor(5,1);
+  lcd.print("Witaj!");
+  enterPinModeActive=0;
+   break;
+   
+  }
+
+  }
+ 
+ aktualnyCzas = millis(); //Pobierz czas, zeby sprawdzic czy wylaczyc buzzer:
+ 
+ if (aktualnyCzas>=buzzer_time)
   {
   digitalWrite(buzzer,LOW);  
   }
   
-  
-   //LCD:
-if (customCheck==1)
-{
-  Serial.println(customKey);
-  if (customKey=='*' || enterPinModeActive==1)
+  if (aktualnyCzas>=lcdFadeTime && lcdFade==1)
   {
-    lcd.clear();
-    lcd.write("Wpisz kod:");
-
-    enterPinModeActive=1;
-    enterPinMode();
-  }
-  
-  if (customKey=='#')
-  {
-   enterPinModeActive=0;
-     digitalWrite(greenLed,HIGH);
-    digitalWrite(redLed,LOW); 
+  lcd.clear();
+  lcd.print("Ekran startowy.");
+  lcd.setCursor(5,1);
+  lcd.print("Witaj!"); 
+  lcdFade=0;
   }
 
- customCheck=0;
 }
-
-}
-
-
-///
 
